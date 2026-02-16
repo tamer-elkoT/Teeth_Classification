6e9d947b37237abdae69eab02b774cf5aae4893c
tamer-elkoT <tamerelkottrinx1.4@gmail.com>
2026-02-16 03:56:10 +0200

    Finish the logic of preprocess image and predict image

--- src/inference.py
+++ src/inference.py
@@ -1,4 +1,4 @@
-from src.model_loader import get_model
+from src.model_loader import get_model, config
 import numpy as np
 import tensorflow as tf
 from io import BytesIO
@@ -18,4 +18,21 @@ def preprocess_image(image_bytes: bytes, target_size=(224,224)):
     # expand the dim
     img_array = tf.expand_dims(img_array, axis=0)
     return img_array
+
+def predict_image(image_bytes: bytes, model_name: str = "CNN"):
+    """
+    This Function predict which class the image belongs to 
+    Parameters:
+    - image_bytes: the uploaded image by the user via streamlit.
+    - model_name: The available models name are ("CNN", "EfficientNetB0")
+    """
+    # load the model 
+    model = get_model(model_name=model_name)
+    # preprocess the input image (image_bytes) >> (check the size )
+    img_array = preprocess_image(image_bytes=image_bytes)
+    predictions = model.predict(img_array)
+    # The np.argmax() get the index of the largest probabilty so in this case
+    predicted_class_index = np.argmax(predictions)
+    # match the predicted index with the class name 
+    return config["classes"][predicted_class_index]
 
--- src/model_loader.py
+++ src/model_loader.py
@@ -10,11 +10,24 @@ from tensorflow.keras.applications.efficientnet import preprocess_input
 # Whenever you see the name 'preprocess_input' in the model file, use this specific function that I just imported from the EfficientNet library.
 custom_dict = {'preprocess_input': preprocess_input}
 
+# 1. Define the Project Root globally so we can use it everywhere
+# __file__ is src/model_loader.py. 
+# .parent is src/ 
+# .parent.parent is the main project folder!
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
 def load_config():
-    # Get the absolute path to the root of the project
-    current_dir = Path(__file__).resolve().parent # return >> /mnt/d/01_Projects/CV/teeth_classification/src
-    # get the config path by 
-    config_path = current_dir.parent / "config" / "config.yaml" # curren_dir.parent >> /mnt/d/01_Projects/CV/teeth_classification
+    """
+    Loads the project configuration settings from the config.yaml file.
+
+    
+    Returns:
+        dict: A dictionary containing the parsed configuration key-value pairs 
+              (e.g., model paths, class labels).
+    """
+   
+    # Use the global PROJECT_ROOT to find the config 
+    config_path = PROJECT_ROOT / "config" / "config.yaml" # curren_dir.parent >> /mnt/d/01_Projects/CV/teeth_classification
     # Open the config file in read only mode and save it tempioraly to the RAM to the file variable
     with open(config_path, "r") as file:
         # yaml.safe_load return a dict of Key-value pair of the yaml file
@@ -22,18 +35,30 @@ def load_config():
 config = load_config()
 
 # load models globally
-CNN_path = config["model_paths"]["cnn"]
-Eff_net_path = config["model_paths"]["efficientnet"]
+rel_path_cnn_model = config["model_paths"]["cnn"]
+rel_path_eff_model = config["model_paths"]["efficientnet"]
+
+CNN_Path = PROJECT_ROOT / rel_path_cnn_model
+EFF_Path = PROJECT_ROOT / rel_path_eff_model
 models = {
-    "CNN": load_model(CNN_path),
-    "EfficientNetB0": load_model(Eff_net_path, custom_objects=custom_dict)# We pass the custom_dict to the effiecenet model to solve the error of preprocess_input not regonized inside the model
+    "CNN": load_model(CNN_Path),
+    "EfficientNetB0": load_model(EFF_Path, custom_objects=custom_dict)# We pass the custom_dict to the effiecenet model to solve the error of preprocess_input not regonized inside the model
 }
 def get_model(model_name: str):
     """
-    Docstring for get_model
-    Load the Model from the RAM
-    :param model_name: Available model names is : (CNN, EfficientNetB0 )
-    :type model_name: str
+    Retrieves a pre-loaded machine learning model from the global memory cache.
+
+    This function acts as a fast-access getter, pulling the fully instantiated 
+    Keras model directly from the server's RAM rather than reloading the heavy 
+    .keras files from the hard drive. This guarantees lightning-fast inference.
+
+    Args:
+        model_name (str): The exact name of the model to retrieve. 
+                          Available options are "CNN" or "EfficientNetB0".
+
+    Returns:
+        tf.keras.Model or None: The compiled Keras model object ready for 
+                                predictions, or None if the model_name is invalid.
     """
     # Get the value of the dictionary (models) >> The model itself after loading it.
     return models.get(model_name)
